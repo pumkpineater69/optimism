2e4e92fc5b5f93602d2019511d98b942afd86107
Kelvin Fichter <kelvinfichter@gmail.com>
2022-04-23 02:41:53 +0200

    feat: introduce chugsplash

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv'
 import { ethers } from 'ethers'
 
 // Hardhat plugins
+import '@eth-optimism/chugsplash'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 import '@nomiclabs/hardhat-etherscan'
@@ -24,7 +25,7 @@ const enableGasReport = !!process.env.ENABLE_GAS_REPORT
 const privateKey = process.env.PRIVATE_KEY || '0x' + '11'.repeat(32) // this is to avoid hardhat error
 const deploy = process.env.DEPLOY_DIRECTORY || 'deploy'
 
-const config: HardhatUserConfig = {
+const config: HardhatUserConfig & any = {
   networks: {
     hardhat: {
       live: false,
